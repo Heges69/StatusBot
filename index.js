22fc60d05e90dfa8b7486032caec4249e0339809
Heges <45619085+Heges69@users.noreply.github.com>
2021-03-19 20:19:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js')
+const client = new Discord.Client();
+const conf = require('./conf.json');
+const UR = require('uptime-robot');
+const ur = new UR(conf.URToken);
+
+client.on('ready', async () => {
+    const channel = await client.channels.fetch(conf.Channel);
+    try {
+        await channel.bulkDelete(100);
+    } catch (e) { }
+    var msg = await channel.send(`**Starting...**`);
+    check(msg);
+    setInterval(()=>{check(msg)}, 120000);
+})
+
+async function check(msg) {
+    var monitors = await ur.getMonitors();
+    var text = ``
+    monitors.forEach(m => {
+        text += `**${m.friendlyname}** - Uptime: ${m.alltimeuptimeratio}%. Status: ${(m.status == 2) ? "<:online:822529778350948452>" : "<:offline:822529831039402024>"}\n\n`
+    })
+    var embd = new Discord.MessageEmbed()
+        .setColor('ffcc4d')
+        .setTitle(`**Status of all of our Systems**`)
+        .setDescription(text);
+    console.log(monitors);
+    msg.edit(embd);
+    msg.edit('');
+}
+
+
+
+
+client.login(conf.Token);
